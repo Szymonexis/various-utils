9b8f487ac57ace4111eebcf4bceb1bc47c8805a9
Szymon Kaszuba-Gałka <hunterqe999.pl@gmail.com>
2026-03-23 17:51:44 +0100

    update

--- beautiful-shell.nix
+++ beautiful-shell.nix
@@ -3,7 +3,7 @@ let
   pkgs = import <nixpkgs> { };
 in
 pkgs.mkShell {
-  # [rust]
+  # [rust]:
   # hardeningDisable = [ "all" ];
   # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
 
@@ -18,7 +18,7 @@ pkgs.mkShell {
     # ]))
 
     # [node]:
-    # nodejs_24
+    nodejs_24
     # prisma-engines_7
     # prisma_7
 
@@ -34,16 +34,17 @@ pkgs.mkShell {
   ];
 
   shellHook = ''
-    # [rust]
+    # [rust]:
     # export NIX_ENFORCE_PURITY=0
 
-    # [node:prisma]
+    # [node:prisma]:
     # export PKG_CONFIG_PATH="${pkgs.openssl.dev}/lib/pkgconfig"
     # export PRISMA_SCHEMA_ENGINE_BINARY="${pkgs.prisma-engines}/bin/schema-engine"
     # export PRISMA_QUERY_ENGINE_BINARY="${pkgs.prisma-engines}/bin/query-engine"
     # export PRISMA_QUERY_ENGINE_LIBRARY="${pkgs.prisma-engines}/lib/libquery_engine.node"
     # export PRISMA_FMT_BINARY="${pkgs.prisma-engines}/bin/prisma-fmt"
 
+    # zsh terminal forwarding:
     export SHELL=${pkgs.zsh}/bin/zsh
     export ZDOTDIR=$(pwd)/.zshrc.d
     mkdir -p $ZDOTDIR
